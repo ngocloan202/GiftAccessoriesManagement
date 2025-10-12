da45b32b990be090c4f3110a8cf565f2bd7ac5dd
LanAnh2324 <lananhnguyen170a@gmail.com>
2025-10-12 16:03:26 +0700

    Fix Entity – Event_Product

--- GiftAccessoriesManagement/Models/Event_Product.cs
+++ GiftAccessoriesManagement/Models/Event_Product.cs
@@ -6,10 +6,10 @@ namespace GiftAccessoriesManagement.Models
     public class Event_Product
     {
         [Key, Column(Order = 0)]
-        public int EventID { get; set; }
+        public int EventId { get; set; }
 
         [Key, Column(Order = 1)]
-        public int ProductID { get; set; }
+        public int ProductId { get; set; }
 
         public virtual Event? Event { get; set; }
         public virtual Product? Product { get; set; }
