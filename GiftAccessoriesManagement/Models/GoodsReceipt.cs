c0d05b87130d185fc01933c36a390be7911277f8
MaiHuong64 <rain06042004@gmail.com>
2025-10-12 07:32:23 +0700

    add GoodsReceipt class with data, properties and annotations

---
+++ GiftAccessoriesManagement/Models/GoodsReceipt.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GiftAccessoriesManagement.Models
+{
+    public class GoodsReceipt
+    {
+        [Key]
+        public int ReceiptId { get; set; }
+        [Required, MaxLength(30)]
+        public int ReceiptCode { get; set; }
+        public DateTime ReceiptDate { get; set; }
+        [Required, MaxLength(255)]
+        public string? SupplierName { get; set; }
+        public string? Note { get; set; }
+        public int EmployeeId { get; set; }
+
+        //public virtual Employee? Employee { get; set; }
+        //public virtual ICollection<GoodsReceiptDetail> GoodsReceiptDetails { get; set; } = new List<GoodsReceiptDetail>();
+    }
+}
