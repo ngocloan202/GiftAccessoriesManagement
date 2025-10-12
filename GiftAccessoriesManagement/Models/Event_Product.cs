114606ceead24899326fba1d7a52763209a17cb5
LanAnh2324 <lananhnguyen170a@gmail.com>
2025-10-12 14:21:28 +0700

    Create Entity – Event_Product

---
+++ GiftAccessoriesManagement/Models/Event_Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GiftAccessoriesManagement.Models
+{
+    public class Event_Product
+    {
+        [Key, Column(Order = 0)]
+        public int EventID { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int ProductID { get; set; }
+
+        public virtual Event? Event { get; set; }
+        public virtual Product? Product { get; set; }
+
+    }
+}
