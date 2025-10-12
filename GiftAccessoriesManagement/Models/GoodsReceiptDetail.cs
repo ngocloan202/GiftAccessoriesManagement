e83eab2b37e381cc847ff2ba0115791c9b387e66
MaiHuong64 <rain06042004@gmail.com>
2025-10-12 08:08:56 +0700

    add GoodsReceiptDetail class and properties

---
+++ GiftAccessoriesManagement/Models/GoodsReceiptDetail.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GiftAccessoriesManagement.Models
+{
+    public class GoodsReceiptDetail
+    {
+        public int GoddsReceiptDetailId { get; set; }
+        public int BatchId { get; set; }
+        public int GoodsReceiptId { get; set; }
+        public int Quantity { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal UnitPrice { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalPrice { get; set; }
+        //public Batch BatchId { get; set; }
+        //public GoodsReceipt GoodsReceiptID { get; set; }
+            
+    }
+}
