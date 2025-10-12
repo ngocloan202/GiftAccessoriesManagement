709295504a671e53a966870369575bfa03483557
ngocloan202 <chauthingocloan0309@gmail.com>
2025-10-12 16:51:58 +0700

    fix eslint

--- GiftAccessoriesManagement/Models/InvoiceDetail.cs
+++ GiftAccessoriesManagement/Models/InvoiceDetail.cs
@@ -10,7 +10,7 @@ namespace GiftAccessoriesManagement.Models
         public int InvoiceId { get; set; }
         public int ProductId { get; set; }
         public int Quantity { get; set; }
-        
+
         [Column(TypeName = "decimal(18,2)")]
         public decimal UnitPrice { get; set; }
 
