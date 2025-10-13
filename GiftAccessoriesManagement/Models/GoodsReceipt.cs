473cc2c5e0e663b6ba1bdf0329d4da81f098a2cb
MaiHuong64 <rain06042004@gmail.com>
2025-10-13 12:44:45 +0700

    rename EmployeeId

---
+++ GiftAccessoriesManagement/Models/GoodsReceipt.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GiftAccessoriesManagement.Models
+{
+    public class GoodsReceipt
+    {
+        [Key]
+        public int GoodsReceiptId { get; set; }
+        [Required, MaxLength(30)]
+        public string? ReceiptCode { get; set; }
+        public DateTime ReceiptDate { get; set; }
+        public string? Note { get; set; }
+        public int EmployeeId { get; set; }
+
+        //public virtual Employee? Employee { get; set; }
+    }
+}
