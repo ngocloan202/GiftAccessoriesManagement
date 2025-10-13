fb1d760a1ddc11b7b67de31c261ce34ede90a238
MaiHuong64 <rain06042004@gmail.com>
2025-10-13 12:51:45 +0700

    fix property class

---
+++ GiftAccessoriesManagement/Models/GoodsReceiptDetail.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GiftAccessoriesManagement.Models
+{
+    public class GoodsReceiptDetail
+    {
+        public int GoodsReceiptDetailId { get; set; }
+        [Required, MaxLength(30)]
+        public int GoodsReceiptId { get; set; }
+        public int ProductId { get; set; }
+        public int QuantityReceived { get; set; }
+        public decimal ImportPrice { get; set; }
+        //public virtual GoodsReceipt? GoodsReceipt { get; set; }
+        public virtual Product? Product { get; set; }
+    }
+}
